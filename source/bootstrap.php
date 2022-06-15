911df9cf2d5204aa21f2ca240c25d483f0a5014c
Giuseppe Spataro <spatyx@gmail.com>
2022-06-15 19:06:40 +0200

    update(bootstrap): add Collision component
    Add nunomaduro/collision component initialization

--- source/bootstrap.php
+++ source/bootstrap.php
@@ -7,3 +7,7 @@ require_once __DIR__ . "/directories.php";
 // Include composer autoloader
 
 require_once DIR_VENDOR . "/autoload.php";
+
+// Initialize nunomaduro/collision component
+
+(new \NunoMaduro\Collision\Provider())->register();
