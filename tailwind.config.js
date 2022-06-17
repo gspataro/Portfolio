2cf09f1334a0c249d2e2d71076e66c2d77b0182e
Giuseppe Spataro <spatyx@gmail.com>
2022-06-17 21:46:52 +0200

    update(tailwind): add headings style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: ["./view/**/*.html"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function({addBase, theme}) {
+        addBase({
+            "h1": {fontSize: theme("fontSize.4xl")},
+            "h2": {fontSize: theme("fontSize.3xl")},
+            "h3": {fontSize: theme("fontSize.2xl")},
+            "h4": {fontSize: theme("fontSize.xl")},
+            "h5": {fontSize: theme("fontSize.lg")}
+        })
+    })
+  ],
 }
