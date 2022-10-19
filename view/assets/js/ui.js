e078e9537275abcbd2c71163a115106f6317cd1c
Giuseppe Spataro <spatyx@gmail.com>
2022-10-19 20:11:25 +0200

    refactor(header): create new header layout

--- view/assets/js/ui.js
+++ view/assets/js/ui.js
@@ -1,14 +1,27 @@
-let navbar = document.getElementById("navbar");
-let navbarOverlay = navbar.getElementsByClassName("overlay")[0];
+document.addEventListener("DOMContentLoaded", () => {
+    /**
+     * Header
+     */
 
-function uiOpenNavbar()
-{
-    navbar.classList.remove("hidden");
-    navbarOverlay.classList.remove("hidden");
-}
+    const header = document.getElementById("header");
+    const headerPlaceholder = document.getElementById("header-placeholder");
 
-function uiCloseNavbar()
-{
-    navbar.classList.add("hidden");
-    navbarOverlay.classList.remove("hidden");
-}
+    headerPlaceholder.style.height = header.clientHeight + "px";
+
+    /**
+     * Navbar
+     */
+
+    const navbar = document.getElementById("navbar");
+    const navbarToggle = document.getElementById("navbar-toggle");
+    const navbarToggleOpen = navbarToggle.getElementsByClassName("open")[0];
+    const navbarToggleClose = navbarToggle.getElementsByClassName("close")[0];
+
+    navbar.style.top = header.clientHeight + "px";
+
+    navbarToggle.onclick = function () {
+        navbarToggleOpen.classList.toggle("hidden");
+        navbarToggleClose.classList.toggle("hidden");
+        navbar.classList.toggle("hidden");
+    }
+});
