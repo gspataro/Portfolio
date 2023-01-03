db8c7558ecf5a1ba20e85328b4b3b2ab7b2d1ce5
Giuseppe Spataro <spatyx@gmail.com>
2023-01-03 16:21:30 +0100

    update(Localization): add Locales
    Add locales class to manage multiple languages in one instance

---
+++ source/components/Localization/Locales.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace GSpataro\Localization;
+
+final class Locales
+{
+    /**
+     * Store languages
+     *
+     * @var array
+     */
+
+    private array $languages = [];
+
+    /**
+     * Add a language
+     *
+     * @param Language $language
+     * @return void
+     */
+
+    public function addLanguage(Language $language): void
+    {
+        if (isset($this->languages[$language->key])) {
+            throw new Exception\LanguageFoundException(
+                "Language already registered: {$language->key}"
+            );
+        }
+
+        $this->languages[$language->key] = $language;
+    }
+
+    /**
+     * Get a language
+     *
+     * @param string $key
+     * @return Language
+     */
+
+    public function getLanguage(string $key): Language
+    {
+        if (!isset($this->languages[$key])) {
+            throw new Exception\LanguageNotFoundException(
+                "Language not found: {$key}"
+            );
+        }
+
+        return $this->languages[$key];
+    }
+}
