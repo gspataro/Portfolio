fbec2d25726e7f187ecea35a67d1d934a2946a5f
Giuseppe Spataro <spatyx@gmail.com>
2023-01-03 15:28:46 +0100

    new(Builder.Page): add page compiler to builder

---
+++ source/components/Builder/Page.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace GSpataro\Builder;
+
+use Twig\Environment as TwigEnvironment;
+
+final class Page
+{
+    /**
+     * Initialize page builder
+     *
+     * @param TwigEnvironment $twig
+     */
+
+    public function __construct(
+        private TwigEnvironment $twig
+    ) {
+    }
+
+    /**
+     * Compile a page
+     *
+     * @param string $templateName
+     * @param string $outputPath
+     * @param array $data
+     * @return void
+     */
+
+    public function compile(string $templateName, string $outputPath, array $data = []): void
+    {
+        $outputPath = DIR_PUBLIC . "/{$outputPath}";
+        $outputDirName = pathinfo($outputPath, PATHINFO_DIRNAME);
+
+        if (!is_dir($outputDirName)) {
+            mkdir($outputDirName);
+        }
+
+        $compiledTemplate = $this->twig->render("{$templateName}.html", $data);
+        file_put_contents($outputPath, $compiledTemplate);
+    }
+}
