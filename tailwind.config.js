72ea87d66ba1bd48d4437a6f69ee1e90341a09eb
Giuseppe Spataro <spatyx@gmail.com>
2023-01-05 11:34:51 +0100

    update(tailwind): update view path

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@
 const plugin = require('tailwindcss/plugin')
 
 module.exports = {
-  content: ["./view/**/*.html"],
+  content: ["./contents/view/**/*.html"],
   theme: {
     colors: {
         "transparent": "transparent",
