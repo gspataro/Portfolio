d80603f234d1b4d07c3e2fcd82ed229165de9de3
Giuseppe Spataro <spatyx@gmail.com>
2023-07-08 12:56:13 +0200

    chore(directories.php): add comments

--- source/directories.php
+++ source/directories.php
@@ -1,10 +1,48 @@
 <?php
 
+/**
+ * Root dir: /
+ */
+
 define("DIR_ROOT", dirname(__DIR__));
+
+/**
+ * Source dir: /source
+ */
+
 define("DIR_SOURCE", DIR_ROOT . "/source");
+
+/**
+ * Third party: /source/vendor
+ */
+
 define("DIR_VENDOR", DIR_SOURCE . "/vendor");
+
+/**
+ * Output dir: /public
+ */
+
 define("DIR_PUBLIC", DIR_ROOT . "/public");
+
+/**
+ * Contents dir: /contents
+ */
 define("DIR_CONTENTS", DIR_ROOT . "/contents");
+
+/**
+ * Data dir: /contents/data
+ */
+
 define("DIR_DATA", DIR_CONTENTS . "/data");
+
+/**
+ * Languages dir: /contents/languages
+ */
+
 define("DIR_LANGS", DIR_CONTENTS . "/languages");
+
+/**
+ * View dir: /contents/view
+ */
+
 define("DIR_VIEW", DIR_CONTENTS . "/view");
