93773369f55ef9ae281fa6152a678032564b4956
Giuseppe Spataro <spatyx@gmail.com>
2023-10-12 18:12:05 +0200

    remove(BuildCommand): remove old components that need restructuring

--- source/components/Command/BuildCommand.php
+++ source/components/Command/BuildCommand.php
@@ -11,17 +11,9 @@ final class BuildCommand extends BaseCommand
     {
         $this->output->print('Running the building process...');
 
-        $blueprint = $this->app->get('app.blueprint');
         $librarian = $this->app->get('library.librarian');
         $architect = $this->app->get('builder.architect');
         $assets = $this->app->get('assets.handler');
-        $locales = $this->app->get('locales');
-        $twig = $this->app->get('twig');
-        $parsedown = $this->app->get('parsedown');
-        $dataBuilder = $this->app->get('builder.data');
-
-        require_once DIR_SOURCE . "/twig_extensions.php";
-
 
         $librarian->run();
         $architect->run();
