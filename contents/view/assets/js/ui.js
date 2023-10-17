5bad9194cd5fe5e8ab668a2e3e1465ae72327a4f
Giuseppe Spataro <spatyx@gmail.com>
2023-10-17 19:10:01 +0200

    update(view.navbar): add basic functionality for mobile navbar

---
+++ contents/view/assets/js/ui.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navbarElement = document.getElementById('navbar');
+    const navbarElementToggle = navbarElement.getElementsByClassName('toggle')[0];
+
+    navbarElementToggle.addEventListener('click', function () {
+        let status = navbarElement.dataset.status;
+
+        if (status == 'open') {
+            navbarElement.dataset.status = 'closed';
+            document.body.style.overflowY = 'auto';
+        } else {
+            navbarElement.dataset.status = 'open';
+            document.body.style.overflowY = 'hidden';
+        }
+    });
+});
