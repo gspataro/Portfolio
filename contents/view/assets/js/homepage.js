4a3843f415d86dbe14d5c049d88a504dc02248f7
Giuseppe Spataro <spatyx@gmail.com>
2023-11-28 20:12:14 +0100

    update(view): introduce alt styles for header

--- contents/view/assets/js/homepage.js
+++ contents/view/assets/js/homepage.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Navigation
 
+    const header = document.getElementById('header');
     const main = document.getElementsByTagName('main')[0];
     const sections = main.getElementsByTagName('section');
     let currentSection = 0;
@@ -23,6 +24,12 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     function goToSection(number) {
+        if (number === 1) {
+            header.dataset.style = 'fortepiano';
+        } else {
+            header.dataset.style = 'pianoforte';
+        }
+
         main.scrollTo({
             left: window.innerWidth * number,
             behavior: 'smooth'
