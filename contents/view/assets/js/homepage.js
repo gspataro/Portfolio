a5ee8b58032bd1272b258e11010fa1c2ca209b21
Giuseppe Spataro <spatyx@gmail.com>
2023-11-28 21:58:47 +0100

    update(view.index): implement navigation

--- contents/view/assets/js/homepage.js
+++ contents/view/assets/js/homepage.js
@@ -4,8 +4,11 @@ document.addEventListener('DOMContentLoaded', function () {
     const header = document.getElementById('header');
     const main = document.getElementsByTagName('main')[0];
     const sections = main.getElementsByTagName('section');
+    const navNext = document.getElementsByClassName('next-section');
+    const navPrev = document.getElementsByClassName('prev-section');
     let currentSection = 0;
 
+    // Arrow keys navigation
     document.addEventListener('keydown', function (e) {
         if (!e.code === 'ArrowRight' && !e.code === 'ArrowLeft') {
             return;
@@ -23,9 +26,30 @@ document.addEventListener('DOMContentLoaded', function () {
         goToSection(currentSection);
     });
 
+    // Navigation
+    for (let i = 0; i < navNext.length; i++) {
+        navNext[i].addEventListener('click', function () {
+            nextSection();
+        });
+    }
+
+    for (let i = 0; i < navPrev.length; i++) {
+        navPrev[i].addEventListener('click', function () {
+            prevSection();
+        });
+    }
+
+    // Reset position on screen resize
     window.addEventListener('resize', function () {
         goToSection(currentSection);
     });
+
+    /**
+     * Go to a specific section
+     *
+     * @param number
+     * @return void
+     */
 
     function goToSection(number) {
         if (number === 1) {
@@ -38,5 +62,37 @@ document.addEventListener('DOMContentLoaded', function () {
             left: window.innerWidth * number,
             behavior: 'smooth'
         });
+    }
+
+    /**
+     * Go to the next section
+     *
+     * @return void
+     */
+
+    function nextSection()
+    {
+        if (currentSection >= (sections.length - 1)) {
+            return;
+        }
+
+        currentSection++;
+        goToSection(currentSection);
+    }
+
+    /**
+     * Go to the previous section
+     *
+     * @return void
+     */
+
+    function prevSection()
+    {
+        if (currentSection <= 0) {
+            return;
+        }
+
+        currentSection--;
+        goToSection(currentSection);
     }
 });
