ab0604242589f42dd3484dd6e6cbb8eceda69601
Giuseppe Spataro <spatyx@gmail.com>
2024-03-31 17:04:04 +0200

    update(header): improve mobile version

--- contents/view/assets/js/ui.js
+++ contents/view/assets/js/ui.js
@@ -1,2 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const header = document.getElementById('header');
+    const navbarToggler = document.getElementById('navbar-toggle');
+
+    navbarToggler.onclick = function () {
+        if (header.dataset.open === 'false') {
+            header.dataset.open = 'true';
+        } else {
+            header.dataset.open = 'false';
+        }
+    };
 });
