a8b6945718ef3ec6664dfaf5ac5440018b06e050
Giuseppe Spataro <spatyx@gmail.com>
2024-04-30 23:16:49 +0200

    update(view): add custom cursor

--- contents/view/assets/js/ui.js
+++ contents/view/assets/js/ui.js
@@ -11,4 +11,35 @@ document.addEventListener('DOMContentLoaded', function () {
             document.body.classList.remove('hamburger-open');
         }
     };
+
+    const customCursor = document.createElement('div');
+    customCursor.style.display = 'none';
+    customCursor.classList.add('cursor');
+    document.body.append(customCursor);
+
+    document.addEventListener('mousemove', function (e) {
+        const xpos = e.clientX - (customCursor.clientWidth / 2);
+        const ypos = e.clientY - (customCursor.clientHeight / 2);
+
+        customCursor.style.display = 'block';
+
+        customCursor.style.top = ypos + 'px';
+        customCursor.style.left = xpos + 'px';
+    });
+
+    document.addEventListener('mouseover', function (e) {
+        if (e.target.tagName.toLowerCase() !== 'a') {
+            return;
+        }
+
+        customCursor.classList.add('hover');
+    });
+
+    document.addEventListener('mouseout', function (e) {
+        if (e.target.tagName.toLowerCase() !== 'a') {
+            return;
+        }
+
+        customCursor.classList.remove('hover');
+    });
 });
