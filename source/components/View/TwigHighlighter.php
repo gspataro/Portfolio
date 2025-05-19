8d7e06ce33f0035fe7e52d79d6a8daebf4cdfe6d
Giuseppe Spataro <spatyx@gmail.com>
2025-05-19 17:35:51 +0200

    update: implement icons

--- source/components/View/TwigHighlighter.php
+++ source/components/View/TwigHighlighter.php
@@ -18,6 +18,19 @@ final class TwigHighlighter extends AbstractExtension
         return $this->highlighter->parse($code, $language);
     }
 
+    public function pregMatch($subject, $pattern)
+    {
+        $matches = [];
+        preg_match($pattern, $subject, $matches);
+
+        return $matches;
+    }
+
+    public function pregReplace($subject, $pattern, $replacement)
+    {
+        return preg_replace($pattern, $replacement, $subject);
+    }
+
     public function getFilters()
     {
         $filters = [];
@@ -25,6 +38,8 @@ final class TwigHighlighter extends AbstractExtension
         $filters[] = new TwigFilter('highlight', [$this, 'highlight'], [
             'is_safe' => ['html']
         ]);
+        $filters[] = new TwigFilter('preg_match', [$this, 'pregMatch']);
+        $filters[] = new TwigFilter('preg_replace', [$this, 'pregReplace']);
 
         return $filters;
     }
