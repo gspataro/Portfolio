75b453d0f256c96a4ae2960ccbb3a6b21c871884
Giuseppe Spataro <spatyx@gmail.com>
2025-06-14 13:14:31 +0200

    update(slider): implement responsive behavior

--- contents/assets/js/slider.js
+++ contents/assets/js/slider.js
@@ -3,10 +3,18 @@ import { Navigation } from "swiper/modules";
 
 const swiper = new Swiper('.swiper', {
     modules: [Navigation],
-    slidesPerView: 3,
-    spaceBetween: 30,
+    slidesPerView: 1,
+    spaceBetween: window.getComputedStyle(document.body).getPropertyValue('--spacing-sm'),
     navigation: {
         prevEl: '.swiper-button-prev',
         nextEl: '.swiper-button-next'
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2
+        },
+        1280: {
+            slidesPerView: 3
+        }
     }
 });
