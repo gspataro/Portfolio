a4b6b99de07afbdfb6855b5eb323b033554588f4
Giuseppe Spataro <spatyx@gmail.com>
2025-07-29 17:58:30 +0200

    update(home): implement slider for blog

--- contents/assets/js/slider.js
+++ contents/assets/js/slider.js
@@ -1,7 +1,27 @@
 import Swiper from "swiper";
 import { Navigation } from "swiper/modules";
 
-const swiper = new Swiper('.swiper', {
+new Swiper('.swiper.slider-services', {
+    modules: [Navigation],
+    slidesPerView: 1,
+    spaceBetween: window.getComputedStyle(document.body).getPropertyValue('--spacing-sm'),
+    navigation: {
+        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next'
+    },
+    breakpoints: {
+        768: {
+            slidesPerGroup: 2,
+            slidesPerView: 2
+        },
+        1280: {
+            slidesPerGroup: 3,
+            slidesPerView: 3
+        }
+    }
+});
+
+new Swiper('.swiper.slider-blog', {
     modules: [Navigation],
     slidesPerView: 1,
     spaceBetween: window.getComputedStyle(document.body).getPropertyValue('--spacing-sm'),
