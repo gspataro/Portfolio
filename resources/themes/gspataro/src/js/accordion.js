3f06e2c110a11d70ce38c9db9e17c9acd6e496de
Giuseppe Spataro <spatyx@gmail.com>
2026-03-22 18:05:19 +0100

    update(accordion): improve functionality

--- resources/themes/gspataro/src/js/accordion.js
+++ resources/themes/gspataro/src/js/accordion.js
@@ -1,31 +1,87 @@
+import { animate } from 'motion';
+
 const accordions = document.getElementsByClassName('accordion');
 
 for (const accordion of accordions) {
-    accordion.querySelectorAll('.item').forEach((item) => {
+    accordion.querySelectorAll('.accordion-item').forEach((item) => {
+        if (!item.getElementsByClassName('accordion-arrow')[0]) {
+            console.error(
+                `Item is missing accordion-arrow`,
+                item
+            );
+            return;
+        }
+
+        if (!item.getElementsByClassName('accordion-content')[0]) {
+            console.error(
+                `Item is missing accordion-content`,
+                item
+            );
+            return;
+        }
+
+        const arrow = item.getElementsByClassName('accordion-arrow')[0];
+        const content = item.getElementsByClassName('accordion-content')[0];
+
         item.accordion = {
-            arrow: item.getElementsByClassName('arrow')[0] ?? null,
+            arrow: arrow,
+            content: content,
+            contentHeight: content.scrollHeight,
             isOpen: () => 'open' in item.dataset,
             open: () => {
-                accordion.querySelectorAll('.item[data-open="open"]').forEach((target) => {
+                accordion.querySelectorAll('.accordion-item[data-open="open"]').forEach((target) => {
                     target.accordion.close();
+                });
+
+                animate(item.accordion.arrow, {
+                    rotate: -180
+                }, {
+                    duration: 0.25,
+                    easing: 'ease-in-out'
+                });
+
+                animate(item.accordion.content, {
+                    height: [0, `${item.accordion.contentHeight}px`]
+                }, {
+                    duration: 0.25,
+                    easing: 'ease-in-out'
                 });
 
                 item.dataset.open = 'open';
             },
-            close: () => delete item.dataset.open
+            close: () => {
+                animate(item.accordion.arrow, {
+                    rotate: 0
+                }, {
+                    duration: 0.25,
+                    easing: 'ease-in-out'
+                });
+
+                animate(item.accordion.content, {
+                    height: [`${item.accordion.contentHeight}px`, 0]
+                }, {
+                    duration: 0.25,
+                    easing: 'ease-in-out'
+                });
+
+                delete item.dataset.open
+            },
+            toggle: () => item.accordion.isOpen() ? item.accordion.close() : item.accordion.open()
         }
 
-        if (item.accordion.arrow !== null) {
-            item.accordion.arrow.addEventListener('click', (e) => {
-                e.preventDefault();
+        accordion.addEventListener('click', (e) => {
+            const toggle = e.target.closest('.accordion-toggle');
 
-                if (item.accordion.isOpen()) {
-                    item.accordion.close();
-                    return;
-                }
+            if (!toggle) {
+                return;
+            }
 
-                item.accordion.open();
-            });
-        }
+            toggle.closest('.accordion-item').accordion.toggle();
+        });
+
+        item.accordion.arrow.addEventListener('click', (e) => {
+            e.preventDefault();
+            item.accordion.toggle();
+        });
     });
 }
